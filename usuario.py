130ce2329cb64d289a6dd124b791f763838ea2f0
EdoRodriguezP <ponce1604@gmail.com>
2025-06-26 00:28:29 -0400

    Se suben archivos del desafio

--- usuario.py
+++ usuario.py
@@ -1,10 +1,10 @@
 class Usuario():
     # Método constructor: inicializa los atributos de la instancia
     def __init__(self, nombre: str, apellido: str, email: str, genero: str) -> None:
-        self.nombre = nombre                # Nombre del usuario
-        self.apellidos = apellido           # Apellido del usuario
-        self.email = email                  # Email del usuario
-        self.genero = genero                # Género del usuario
+        self.nombre = nombre                
+        self.apellidos = apellido           
+        self.email = email                  
+        self.genero = genero               
 
     # Representación en texto de la instancia (para imprimir)
     def __repr__(self):
